57e340d97680ce2709940f4a8779070d41a5df20
Okinbird <okinyinka@yahoo.co.uk>
2022-12-15 14:00:13 -0800

    binary tree

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,46 +1,79 @@
 #include "binary_trees.h"
-#include "0-binary_tree_node.c"
 
 /**
- * heap_extract - extracts the root node from a Max Binary Heap
- * @root: pointer to the heap root
- * Return: value of extracted node
- **/
+ * max - Finds the maximum node in a tree.
+ * @tree: The pointer to the root of the tree.
+ * Return: The node with the maximum value.
+ */
+heap_t *max(heap_t *tree)
+{
+	heap_t *curr_max, *left_max, *right_max;
+
+	if (!tree->left)
+		return (tree);
+	left_max = max(tree->left);
+	if (left_max->n > tree->n)
+		curr_max = left_max;
+	else
+		curr_max = tree;
+	if (tree->right)
+	{
+		right_max = max(tree->right);
+		if (right_max->n > curr_max->n)
+			curr_max = right_max;
+		else
+			curr_max = tree;
+	}
+	return (curr_max);
+}
+
+/**
+ * recurse_extract - Recursively extracts the max from the tree.
+ * @tree: The pointer to the root of the tree.
+ */
+void recurse_extract(heap_t *tree)
+{
+	heap_t *sub_max, *right_max = NULL;
+
+	if (!tree->left)
+		return;
+	sub_max = max((tree)->left);
+	if (tree->right)
+		right_max = max(tree->right);
+	if (right_max && right_max->n > sub_max->n)
+		sub_max = right_max;
+	tree->n = sub_max->n;
+	if (!sub_max->left)
+	{
+		if (sub_max->parent && sub_max->parent->left == sub_max)
+			sub_max->parent->left = NULL;
+		if (sub_max->parent && sub_max->parent->right == sub_max)
+			sub_max->parent->right = NULL;
+		free(sub_max);
+	}
+	else
+		recurse_extract(sub_max);
+}
+
+/**
+ * heap_extract - Extracts the root from a Binary Heap.
+ * @root: The pointer to the root of the tree.
+ * Return: The value of the extracted node.
+ */
 int heap_extract(heap_t **root)
 {
-	int value, value_aux;
-	heap_t *aux;
+	int value;
 
-	if (!root || !*root)
+	if (!*root)
 		return (0);
-	aux = *root;
-	value = aux->n;
-	if (!aux->left && !aux->right && !aux->parent)
+	value = (*root)->n;
+	if (!(*root)->left)
 	{
-		free(aux);
+		value = (*root)->n;
+		free(*root);
+		*root = NULL;
 		return (value);
 	}
-	while (aux->left || aux->right)
-	{
-		if (aux->left && aux->right)
-		{
-			if (!aux->right || aux->left->n > aux->right->n)
-			{
-				value_aux = aux->n;
-				aux->n = aux->left->n;
-				aux->left->n = value_aux;
-				aux = aux->left;
-			}
-			else if (aux->left->n < aux->right->n)
-			{
-				value_aux = aux->n;
-				aux->n = aux->right->n;
-				aux->right->n = value_aux;
-				aux = aux->right;
-
-			}
-		}
-
-	}
+	recurse_extract(*root);
 	return (value);
 }
